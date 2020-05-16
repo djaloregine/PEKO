5a209f2789af220d69c83095d460f782ae1278c2
Regine Djalo <djalo50@gmail.com>
2020-05-16 16:47:54 +0200

    likes

--- backend/controllers/produits.js
+++ backend/controllers/produits.js
@@ -36,9 +36,8 @@ exports.getOneSauce = (req, res) => {
 };
 
 // Attention WILL commence ici par créer un nouvel objet ! 
-exports.modifySauce = (req, res) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-   sauceObject = req.file ? {
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
         imageURL: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : {
@@ -47,11 +46,11 @@ exports.modifySauce = (req, res) => {
     Sauce.updateOne({
             _id: req.params.id
         }, {
-            ...req.body,
+            ...sauceObject,
             _id: req.params.id
         })
         .then(() => res.status(200).json({
-            message: 'tralalaire'
+            message: 'sauce modifiée'
         }))
         .catch((error) => res.status(404).json({
             error
@@ -89,3 +88,38 @@ exports.getAllSauce = (req, res) => {
             message: " ras le bol"
         }));
 };
+
+
+exports.createLikes = (req, res, next) => {
+    const action = req.body.action;
+    const userLikes = [userId];
+    const likesCount = userLikes.length + 1
+    likesCount = 0
+    const userDislikes = [userId]
+    const dislikesCount = userDislikes.length + 1
+    dislikesCount = 0
+
+    const likeCount = action === 'Like' ? 1 : -1;
+    
+    createLikes.update({
+        _id: req.params.id
+    }, {
+        $inc: {
+            likes_count: counter
+        }
+    }, {}, (err, numberAffected) => {
+        res.send('');
+    });
+
+    // le user se connecte 
+    // trouver une sauce sauce.findOne
+    // cliquer sur un bouton (1, 0, -1) 
+    // if (likes === 1) {le userId est ajouté au tableau de ceux qui aiment} // le tableau du userID est mis à jour // le nombre de like est augmenté de 1
+    // else if (likes === 0) {le _userId est enlevé soit au tableau de ceux qui aiment, soit au tableau de ceux qu n'aiment pas} //  le tableau du userID est mis à jour // le nombre de like ou dislike est diminué de 1 // 
+    // else càd -1, le _userId est ajouté au tableau de ceux qui n'aiment pas // le tableau du userID est mis à jour // le nombre de dislike est augmenté de 1
+    // le _userId ne doit avoir la possibilité d'utiliser sa voix qu'une seule fois sur une sauce 
+
+
+
+    // https://gist.github.com/aerrity/fd393e5511106420fba0c9602cc05d35
+}
