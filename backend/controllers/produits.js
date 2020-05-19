5181e407c9044c2e0c06e57f40c541ec5a3fa43a
Regine Djalo <djalo50@gmail.com>
2020-05-19 20:17:34 +0200

    likes

--- backend/controllers/produits.js
+++ backend/controllers/produits.js
@@ -90,59 +90,91 @@ exports.getAllSauce = (req, res) => {
 };
 
 
-
 exports.likeSauce = (req, res) => {
-    const action = 0;
-    Sauce.updateOne({
-            _id: req.params.id,
-        }, {
-            ...req.body,
-            _id: req.params.id
-        }).then(() => {
-            userId: req.body.userId
-            if (action === 1) {
-                likes: req.body.likes === 0
-                usersLiked: req.body.usersLiked
-                $addToSet: {
-                    usersLiked,
-                    1
-                }
-                $inc: {
-                    likes,
-                    1
-                }
-            }
-            else if (action === -1) {
-                dislikes: req.body.dislikes === 0
-                usersDisliked: req.body.usersDisliked
-                $addToSet: {
-                    usersDisliked,
-                    1
-                }
-                $inc: {
-                    dislikes,
-                    1
-                }
-            }
-            else {
-                if (usersLiked.indexOf(userId)) {
-                    const place = usersLiked.indexOf('userId');
-                    const userMin = usersLiked.splice(place, 1);
-                    $pull: {
-                        usersLiked,
-                        userMin
+
+    Sauce.findById(req.params.id, (error, data) => {
+        let sauce = data
+        switch (req.body.like) {
+            case 1:
+                sauce['likes'] = sauce['likes'] ? sauce['likes'] : 0
+                sauce['dislikes'] = sauce['dislikes'] ? sauce['dislikes'] : 0
+                if (sauce.usersLiked.indexOf(req.body.userId) == -1) {
+                    const place = sauce.usersDisliked.indexOf(req.body.userId);
+                    if (place != -1) {
+                        sauce.usersDisliked.splice(place, 1);
+                        sauce.dislikes--;
                     }
-                } else if (usersDisliked.indexOf(userId)) {
-                    const place = usersDisliked.indexOf('userId');
-                    const userMin = usersDisliked.splice(place, 1);
-                    $pull: {
-                        usersDisliked,
-                        userMin
-                    }
+                    sauce.usersLiked.push(req.body.userId)
+                    sauce.likes++;
+
                 }
-            }
-        })
-        .catch((error) => res.status(500).json({
-            message: 'tu es sur la bonne erreur'
-        }));
+                break
+
+        }
+
+        Sauce.updateOne({
+                _id: req.params.id
+            }, sauce).then(() => res.status(200).json({
+                message: 'sauce modifiée'
+            }))
+            .catch((error) => res.status(404).json({
+                error
+            }));
+    })
+
 }
+
+
+/*  Sauce.updateOne({
+          _id: req.params.id,
+      }, {
+          ...req.body,
+          _id: req.params.id
+      }).then(() => {
+          userId = req.body.userId
+          if (action === 1) {
+              likes: req.body.likes === 0
+              usersLiked: req.body.usersLiked
+              $addToSet: {
+                  usersLiked,
+                  1
+              }
+              $inc: {
+                  likes,
+                  1
+              }
+          }
+          else if (action === -1) {
+              dislikes: req.body.dislikes === 0
+              usersDisliked: req.body.usersDisliked
+              $addToSet: {
+                  usersDisliked,
+                  1
+              }
+              $inc: {
+                  dislikes,
+                  1
+              }
+          }
+          else {
+              if (usersLiked.indexOf(userId)) {
+                  const place = usersLiked.indexOf('userId');
+                  const userMin = usersLiked.splice(place, 1);
+                  $pull: {
+                      usersLiked,
+                      userMin
+                  }
+              } else if (usersDisliked.indexOf(userId)) {
+                  const place = usersDisliked.indexOf('userId');
+                  const userMin = usersDisliked.splice(place, 1);
+                  $pull: {
+                      usersDisliked,
+                      userMin
+                  }
+              }
+          }
+      })
+      .catch((error) => res.status(500).json({
+          message: 'tu es sur la bonne erreur'
+      }));
+      */
