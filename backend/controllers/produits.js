0365e62b410f90f6523a62c7b994e45677af2ead
Regine Djalo <djalo50@gmail.com>
2020-06-03 15:30:10 +0200

    likes

--- backend/controllers/produits.js
+++ backend/controllers/produits.js
@@ -94,28 +94,34 @@ exports.likeSauce = (req, res) => {
         let sauce = data // collection = sauces sur MongoDB
         switch (req.body.like) {
             case 1:
-                sauce['likes'] = sauce['likes'] ? sauce['likes'] : 0 // remet compteur à 0
-                sauce['dislikes'] = sauce['dislikes'] ? sauce['dislikes'] : 0
-                if (sauce.usersLiked.indexOf(req.body.userId) == -1) {
-                    const place = sauce.usersDisliked.indexOf(req.body.userId);
-                    // attention à la double négation ci-dessous
-                    if (place != -1) {
-                        sauce.usersDisliked.splice(place, 1);
-                        sauce.dislikes--;
+                sauce['likes'] ? sauce['likes'] : 0;
+                // si je donne la propriété likes soit j'ajoute like à l'objet sauce, soit je reviens à 0
+                if (sauce['usersLiked'].indexOf(req.body.userId) == -1) {
+                    /* indexOf == -1 c'est que l'élément recherché n'est pas présent dans le tableau des usagers aimant la sauce*/
+                    if (sauce['usersDisliked'].indexOf(req.body.userId)) {
+                        const place = sauce.usersDisliked.indexOf(req.body.userId);
+                        // je demande l'emplacement dans le tableau des usagers qui n'ont pas aimé de l'ID de celui qui n'a pas aimé 
+                        if (place != -1) {
+                            // (place != -1) c'est que j'ai trouvé qqu'un dans le tableau de ceux qui n'ont pas aimé 
+                            sauce['usersDisliked'].splice(place);
+                            // je supprime cet usager qui n'a pas aimé du tableau 
+                            sauce.dislikes--;
+                            // je décrémente le tableau de cequi qui n'ont pas aimé 
+                        }
+                        sauce['usersLiked'].push(req.body.userId)
+                        sauce.likes++;
                     }
-                    sauce.usersLiked.push(req.body.userId)
-                    sauce.likes++;
-
                 }
                 break;
             case -1:
+                sauce['dislikes'] ? sauce['dislikes'] : 0;
                 if (sauce.usersDisliked.indexOf(req.body.userId) == -1) {
                     const place = sauce.usersLiked.indexOf(req.body.userId);
-                    if (place != -1) {
+                    if (place != -2) {
                         sauce.usersLiked.splice(place, 1);
                         sauce.likes--;
                     }
-                    sauce.usersDisliked.push(req.body.userId)
+                    sauce.usersDisliked.push(req.body.userId);
                     sauce.dislikes++;
                 }
                 case 0:
@@ -126,8 +132,8 @@ exports.likeSauce = (req, res) => {
                             sauce.likes--;
                         } else {
                             const place = sauce.usersDisliked.indexOf(req.body.userId);
-                            sauce.usersDisliked.splice(place, 1);
-                            sauce.dislikes--;
+                            sauce.usersDisliked.splice(place, -1);
+                            sauce.dislikes++;
                         }
                     }
         }
@@ -141,3 +147,60 @@ exports.likeSauce = (req, res) => {
             }));
     })
 }
+
+/*
+exports.likeSauce = (req, res) => {
+    Sauce.findById(req.params.id, (error, data) => {
+        let sauce = data // collection = sauces sur MongoDB
+        switch (req.body.like) {
+            case 1:
+                sauce['likes'] ? sauce['likes'] : 0;
+               
+
+                if (sauce['usersLiked'].indexOf(req.body.userId) == -1) {
+                    
+                    if(sauce['usersLiked'].indexOf(req.body.userId) == -1)
+                    const place = sauce.usersDisliked.indexOf(req.body.userId);
+                    // attention à la double négation ci-dessous
+                    if (place != -1) {
+                        sauce.usersDisliked.splice(place, 1);
+                        sauce.dislikes--;
+                    }
+                    sauce.usersLiked.push(req.body.userId)
+                    sauce.likes++;
+                }
+                break;
+            case -1:
+                sauce['dislikes'] ? sauce['dislikes'] : 0;
+                if (sauce.usersDisliked.indexOf(req.body.userId) == -1) {
+                    const place = sauce.usersLiked.indexOf(req.body.userId);
+                    if (place != -2) {
+                        sauce.usersLiked.splice(place, 1);
+                        sauce.likes--;
+                    }
+                    sauce.usersDisliked.push(req.body.userId);
+                    sauce.dislikes++;
+                }
+                case 0:
+                    if (sauce.usersDisliked.indexOf(req.body.userId)) {
+                        const place = sauce.usersLiked.indexOf(req.body.userId);
+                        if (place != -1) {
+                            sauce.usersLiked.splice(place, 1);
+                            sauce.likes--;
+                        } else {
+                            const place = sauce.usersDisliked.indexOf(req.body.userId);
+                            sauce.usersDisliked.splice(place, -1);
+                            sauce.dislikes++;
+                        }
+                    }
+        }
+        Sauce.updateOne({
+                _id: req.params.id
+            }, sauce).then(() => res.status(200).json({
+                message: 'sauce modifiée'
+            }))
+            .catch((error) => res.status(404).json({
+                error
+            }));
+    })
+} */
--- backend/controllers/usagers.js
+++ backend/controllers/usagers.js
@@ -63,7 +63,7 @@ exports.login = (req, res, next) => {
                         token: jwt.sign({
                             userId: user._id
                         }, 'secret', {
-                            expiresIn: '24h'
+                            expiresIn: '1h'
                         })
                     });
                 })
